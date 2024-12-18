5fc662083e9b442f343ebb9b7a782626b0385510
Nathaniel Tomsett <nathaniel.tomsett@gmail.com>
2024-12-18 23:01:28 +0000

    remove duplicated rendering of a comment, seems like a framework bug

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -24,23 +24,3 @@
         <p>No posts available.</p>
     @endif
 @endsection
-
-
-
-
-
-
-
-
-<!-- @extends('layouts.app')
-
-@section('title', 'All Posts')
-
-@section('content')
-    <h1>All Posts</h1>
-    @foreach ($posts as $post)
-        <p>{{ $post->title }} - {{ $post->user->name ?? 'Unknown User' }}</p>
-    @endforeach
-
-    {{ $posts->links() }}
-@endsection -->
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,21 +10,3 @@
     
     <a href="{{ route('posts.index') }}">Back to Posts</a> 
 @endsection
-
-
-
-
-
-
-<!-- @extends('layouts.app') //Assuming you use a layout
-
-@section('title', 'Post Details')
-
-@section('content')
-    <h1>{{ $post->title }}</h1>
-    <p>{{ $post->body }}</p>
-    <p><strong>Posted by:</strong> {{ $post->user->name ?? 'Unknown User' }}</p>
-    <p><strong>Created at:</strong> {{ $post->created_at->format('F j, Y') }}</p>
-
-    <a href="{{ route('posts.index') }}">Back to Posts</a>
-@endsection -->
